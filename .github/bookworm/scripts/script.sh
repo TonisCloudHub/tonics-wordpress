0910b053abf261076785f04eccbcb820630292f5
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2024-04-20 12:41:32 +0100

    Update script.sh

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -44,9 +44,9 @@ sudo incus exec tonics-wordpress -- bash -c "tar xvzf wordpress.tar.gz"
 WordPress_Version=$(sudo incus exec tonics-wordpress -- grep '$wp_version =' wordpress/wp-includes/version.php | awk -F"'" '{print $2}')
 
 # Create the target directory if it doesn't exist
-sudo incus exec tonics-wordpress -- bash -c "mkdir -p /var/www/html/"
+sudo incus exec tonics-wordpress -- bash -c "mkdir -p /var/www/wordpress/"
 
-sudo incus exec tonics-wordpress -- bash -c "mv wordpress/* /var/www/html/"
+sudo incus exec tonics-wordpress -- bash -c "mv wordpress/* /var/www/wordpress/"
 
 # Version
 Version="MariaDB__$(sudo incus exec tonics-wordpress -- mysql -V | awk '{print $5}' | sed 's/,//')__Nginx__$(sudo incus exec tonics-wordpress -- nginx -v |& sed 's/nginx version: nginx\///')__PHP__$(sudo incus exec tonics-wordpress -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)__WordPress__$WordPress_Version"
